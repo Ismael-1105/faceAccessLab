6d535dcd6d82b77fa9754486a0c2e43fe3748bdd
Ismael-1105 <gonzalezismael5711@gmail.com>
2026-07-30 15:35:31 -0500

    migracion a librerias de next

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,27 @@
 import js from '@eslint/js';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
 
 export default tseslint.config(
   {
-    ignores: ['dist', 'node_modules', '.vite'],
+    ignores: ['dist', 'node_modules', '.next', '.vercel', '.codegraph', '.opencode'],
   },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2022,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
       '@typescript-eslint/no-unused-vars': 'off',
     },
   },
